0b5d32deabb71ae33db2c0fa6d6b1ca7a9924aac
imayakulothungan <g52opal@yahoo.com>
2018-09-05 07:19:30 -0400

    sysout to logger

--- src/main/java/uiak/exper/batch/framework/ConsoleItemWriter.java
+++ src/main/java/uiak/exper/batch/framework/ConsoleItemWriter.java
@@ -1,20 +1,23 @@
 package uiak.exper.batch.framework;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemWriter;
 import java.util.List;
 
 
 public class ConsoleItemWriter<T> implements ItemWriter<T> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ConsoleItemWriter.class);
 
     @Override
     public void write(List<? extends T> items) throws Exception {
-
+        LOG.trace("Console item writer starts");
         for (T item : items) {
-            System.out.println("CONSOLE ITEM WRITER " + item);
+            LOG.info("CONSOLE ITEM WRITER " + item);
         }
-
+        LOG.trace("Console item writer ends");
 
     }
 }
