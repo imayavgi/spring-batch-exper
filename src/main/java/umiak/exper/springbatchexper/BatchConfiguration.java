2a59ff4373d2c44a7074516bd2db9321b59aea59
imayakulothungan <g52opal@yahoo.com>
2018-09-06 06:24:31 -0400

    pg job repo

--- src/main/java/umiak/exper/springbatchexper/BatchConfiguration.java
+++ src/main/java/umiak/exper/springbatchexper/BatchConfiguration.java
@@ -11,6 +11,8 @@ import org.springframework.batch.item.support.PassThroughItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import uiak.exper.batch.framework.ConsoleItemWriter;
 import uiak.exper.batch.framework.CustomJobExecListener;
 import uiak.exper.batch.framework.InvoiceItemReaderFromYaml;
@@ -54,5 +56,16 @@ public class BatchConfiguration {
                 .processor(new PassThroughItemProcessor())
                 .writer(new ConsoleItemWriter<Invoice>())
                 .build();
+    }
+
+
+    @Bean(name="dataSource")
+    public DriverManagerDataSource dataSource(Environment env){
+        DriverManagerDataSource dataSource= new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("batch.jdbc.driver"));
+        dataSource.setUrl(env.getProperty("batch.jdbc.url"));
+        dataSource.setUsername(env.getProperty("batch.jdbc.user"));
+        dataSource.setPassword(env.getProperty("batch.jdbc.password"));
+        return dataSource;
     }
 }
